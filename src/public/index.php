f1e42cd6fdc46c6d32b5b4f2a26fbdab92de8526
Michael West <michael.west@alig2216.staff.aligent.consulting>
2022-05-18 13:29:16 +0930

    push lesson 2.4, namespaces

--- src/public/index.php
+++ src/public/index.php
@@ -1,70 +1,24 @@
 <?php
 
-require_once 'PaymentProfile.php';
-require_once 'Customer.php';
-require_once 'Transaction.php';
-
-
-$transaction = new Transaction(5, 'Test');
-
-//$profileId = null;
-//
-//if($customer = $transaction->getCustomer()) {
-//    if($paymentProfile = $customer->getPaymentProfile()) {
-//        $profileId = $paymentProfile->id;
-//    }
-//}
-//
-//echo $profileId;
-
-
-echo $transaction->getCustomer()?->setPaymentProfile(createProfile())?->id;
-
-
-
-//declare(strict_types=1);
+//require_once 'PaymentProfile.php';
+//require_once 'Customer.php';
 //require_once 'Transaction.php';
 //
-////Classes and Objects
-//
-//$transaction = (new Transaction(100, 'Transaction 1'))
-//    -> addTax(8)
-//    -> applyDiscount( 10);
-//
-//
-////$transaction2 = (new Transaction(200, 'Transaction 2'))
-////    -> addTax(8)
-////    -> applyDiscount( 15);
-////
-//
-//
-//$amount = $transaction->getAmount();
-//
-//exit;
-//
-//var_dump($amount);
+
+require_once '../PaymentGateway/Stripe/Transaction.php';
+require_once '../PaymentGateway/Paddle/Transaction.php';
+require_once '../PaymentGateway/Paddle/CustomerProfile.php';
+require_once '../Notification/Email.php';
 
 
-
-//$str = '{"a":1,"b":2,"c":3}';
-//
-//$arr = json_decode($str);
-//
-//var_dump($arr->c);
+use PaymentGateway\Paddle\{Transaction, CustomerProfile};
+use PaymentGateway\Stripe\Transaction as StripeTransaction;
 
 
-//$obj = new \stdClass();
-//$obj->a = 1;
-//$obj->b = 2;
-//
-//var_dump($obj);
+$paddleTransaction = new Transaction();
+$stripeTransaction = new StripeTransaction();
+$paddleCustomerProfile = new CustomerProfile();
 
-//
-//$arr = [1, 2, 3];
-//$obj = (object) $arr;
-//var_dump($obj->{0});
 
-//$obj = (object) 1;
-//var_dump($obj->scalar);
+var_dump($paddleTransaction, $stripeTransaction, $paddleCustomerProfile);
 
-//done
