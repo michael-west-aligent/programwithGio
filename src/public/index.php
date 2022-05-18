d688dfff0e474995668102ad31b4bff4a0ab2274
Michael West <michael.west@alig2216.staff.aligent.consulting>
2022-05-18 11:07:22 +0930

    push lesson 2.2

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,45 @@
 <?php
+//declare(strict_types=1);
+//require_once 'Transaction.php';
+//
+////Classes and Objects
+//
+//$transaction = (new Transaction(100, 'Transaction 1'))
+//    -> addTax(8)
+//    -> applyDiscount( 10);
+//
+//
+////$transaction2 = (new Transaction(200, 'Transaction 2'))
+////    -> addTax(8)
+////    -> applyDiscount( 15);
+////
+//
+//
+//$amount = $transaction->getAmount();
+//
+//exit;
+//
+//var_dump($amount);
 
-//phpinfo();
 
-echo '<pre>';
-print_r($_SERVER);
-echo '</pre>';
+
+//$str = '{"a":1,"b":2,"c":3}';
+//
+//$arr = json_decode($str);
+//
+//var_dump($arr->c);
+
+
+//$obj = new \stdClass();
+//$obj->a = 1;
+//$obj->b = 2;
+//
+//var_dump($obj);
+
+//
+//$arr = [1, 2, 3];
+//$obj = (object) $arr;
+//var_dump($obj->{0});
+
+$obj = (object) 1;
+var_dump($obj->scalar);
