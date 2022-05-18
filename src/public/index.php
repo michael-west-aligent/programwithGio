49c84295e0c8648e25519f5c2439d0e2555018a7
Michael West <michael.west@alig2216.staff.aligent.consulting>
2022-05-18 15:36:54 +0930

    2.5 done

--- src/public/index.php
+++ src/public/index.php
@@ -5,20 +5,28 @@
 //require_once 'Transaction.php';
 //
 
-require_once '../PaymentGateway/Stripe/Transaction.php';
-require_once '../PaymentGateway/Paddle/Transaction.php';
-require_once '../PaymentGateway/Paddle/CustomerProfile.php';
-require_once '../Notification/Email.php';
+require_once '../app/PaymentGateway/Stripe/Transaction.php';
+require_once '../app/PaymentGateway/Paddle/Transaction.php';
+require_once '../app/PaymentGateway/Paddle/CustomerProfile.php';
+require_once '../app/Notification/Email.php';
 
 
-use PaymentGateway\Paddle\{Transaction, CustomerProfile};
-use PaymentGateway\Stripe\Transaction as StripeTransaction;
+//require_once '../../app/PaymentGateway/Stripe/Transaction.php';
+//require_once '../../app/PaymentGateway/Paddle/Transaction.php';
+//require_once '../../app/PaymentGateway/Paddle/CustomerProfile.php';
+//require_once '../../app/Notification/Email.php';
 
+//
+//use App\PaymentGateway\Paddle\{Transaction, CustomerProfile};
+//use App\PaymentGateway\Stripe\Transaction as StripeTransaction;
+
+
+use App\PaymentGateway\Paddle\Transaction;
 
 $paddleTransaction = new Transaction();
-$stripeTransaction = new StripeTransaction();
-$paddleCustomerProfile = new CustomerProfile();
+//$stripeTransaction = new StripeTransaction();
+//$paddleCustomerProfile = new CustomerProfile();
 
 
-var_dump($paddleTransaction, $stripeTransaction, $paddleCustomerProfile);
+var_dump($paddleTransaction);
 
