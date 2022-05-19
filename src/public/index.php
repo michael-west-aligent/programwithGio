a8966ccc7e2ffbb62c2a74d57e8fbfccf72d8946
Michael West <michael.west@alig2216.staff.aligent.consulting>
2022-05-19 14:51:54 +0930

    interfaces and polymorphism

--- src/public/index.php
+++ src/public/index.php
@@ -2,14 +2,21 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$fields = [
-//    new \App\Field('basefield'),
-    new \App\Text('textField'),
-//    new \App\Boolean('booleanField'),
-    new \App\Checkbox('checkboxField'),
-    new \App\Radio('radioField'),
-];
+$service = new App\DebtCollectionService();
 
-foreach($fields as $field) {
-    echo $field->render() . '<br />';
-}
+echo $service->collectDebt(new App\Rocky()) . PHP_EOL;
+
+
+
+
+//$fields = [
+////    new \App\Field('basefield'),
+//    new \App\Text('textField'),
+////    new \App\Boolean('booleanField'),
+//    new \App\Checkbox('checkboxField'),
+//    new \App\Radio('radioField'),
+//];
+//
+//foreach($fields as $field) {
+//    echo $field->render() . '<br />';
+//}
