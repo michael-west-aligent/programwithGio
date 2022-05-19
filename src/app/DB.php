0c58f9f6f64105c026901f5111389864cf2406f1
Michael West <michael.west@alig2216.staff.aligent.consulting>
2022-05-19 10:46:36 +0930

    lesson done, staic props and methods in objects

---
+++ src/app/DB.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+class DB
+{
+    public static ?DB $instance = null;
+
+    public function __construct(public array $config)
+    {
+        echo 'instance created <br/> ';
+    }
+
+    public static function getInstance(array $config):DB
+    {
+        if(self::$instance == null) {
+            self::$instance = new DB($config);
+        }
+
+        return self::$instance;
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -1,12 +1,18 @@
 <?php
 
+use App\DB;
 use App\PaymentGateway\Paddle\Transaction;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$transaction = new Transaction();
+$transaction = new Transaction( 25, 'Transaction 1');
 
-$transaction->setStatus(\App\Enums\Status::PAID);
+//$db = DB::getInstance([]);
+//
+//$transaction = TransactionFactory::make(25, 'Transaction 1');
 
-var_dump($transaction);
+$transaction->process();
+//
+//var_dump($transaction::getCount());
 
+var_dump($transaction->amount);
