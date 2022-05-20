93e8b25a3e6ac3be9e310c1905f08db43a6c49f6
Michael West <michael.west@alig2216.staff.aligent.consulting>
2022-05-20 12:32:45 +0930

    docblok, glanced over

--- src/public/index.php
+++ src/public/index.php
@@ -4,13 +4,3 @@ use App\AnInvoice;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$invoice1 = new AnInvoice(25, 'My invoice 1');
-$invoice2 = new AnInvoice(100, 'My invoice 2');
-
-$invoice3 = $invoice1;
-
-echo 'invoice 1 == invoice 3' . PHP_EOL;
-var_dump($invoice1 == $invoice3);
-
-echo 'invoice1 === invoice 2' . PHP_EOL;
-var_dump($invoice1 === $invoice3q);
