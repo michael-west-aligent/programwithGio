f87777215127cbd6719e76937fc9ca2d0ac7195e
Michael West <michael.west@alig2216.staff.aligent.consulting>
2022-05-20 12:53:58 +0930

    object cloning

--- src/app/Invoice.php
+++ src/app/Invoice.php
@@ -1,39 +1,26 @@
 <?php
 
-//namespace App;
-//
-//class Invoice
-//{
-//
-//    protected array $data;
-//
-//    public function __get(string $name)
-//    {
-//if (array_key_exists($name, $this->data)){
-//    return $this->data[$name];
-//}
-//    return null;
-//    }
-//
-//    public function  __set(string $name, $value): void
-//    {
-//        $this->data[$name] = $value;
-//    }
-//
-//    /** isset is called when you use eset or empty function on undefined or inaccessible properties
-//     *needs to return a boolean value
-//     */
-//    public function __isset(string $name): bool
-//    {
-//        // TODO: Implement __isset() method.
-//    }
-//
-//    /** unset methods get called when you use the unset function onundefined properties   */
-//    public function __unset(string $name): void
-//    {
-//        // TODO: Implement __unset() method.
-//    }
-//
-//
-//}
+namespace App;
 
+class Invoice
+{
+    private string $id;
+
+    public function __construct()
+    {
+        $this->id = uniqid('invoice');
+
+        var_dump('__construct');
+    }
+
+    public function __clone(): void
+    {
+        $this->id = uniqid('invoice');
+        var_dump('__clone');
+    }
+
+//    public static function create(): static
+//    {
+//        return new static();
+//    }
+}
