7abcbbd21587c0b39d978743fabbe77e036689d1
Michael West <michael.west@alig2216.staff.aligent.consulting>
2022-05-21 16:39:14 +0930

    lesson done, may need to re cover

--- src/public/index.php
+++ src/public/index.php
@@ -1,15 +1,24 @@
 <?php
-
+use App\Invoice;
+use App\InvoiceCollection;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+//array
+//foreach(['a', 'b', 'c', 'd', 'e'] as $key => $value) {
 
-$dateTime = new DateTime();
+//replace array with an object of invoice class
+//
 
-//var_dump($dateTime);
+//foreach(new App\Invoice(25) as $key=> $value)  {
+//
+//    //we get the id number and value amount
+//    echo $key . ' = ' . $value . PHP_EOL;
+//}
 
-$dateTime->setTimezone(new DateTimeZone('Australia/Adelaide'));
+$invoiceCollection = new InvoiceCollection([new Invoice(15), new Invoice(25), new Invoice(50)]);
 
-//var_dump($dateTime);
-
-echo $dateTime->format('m/d/Y g:i A') . PHP_EOL;
+foreach($invoiceCollection as $invoice ) {
+//    var_dump($invoice);
+    echo $invoice->id . '-' . $invoice->amount .  PHP_EOL;
+}
